5482db60dadc1a49372a30a60fd95a97d714335b
yakovyshynVitalii <vakovisin94@gmail.com>
2023-02-05 17:10:07 +0200

    add module nuxt device

---
+++ project/pages/device.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ activeDevice }}
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+// щоб використовувати достатньо вивести необхідну змінну з useDevice()
+// детальніше https://www.npmjs.com/package/@nuxtjs/device
+
+const { isMobileOrTablet } = useDevice();
+
+const activeDevice = computed(() =>
+  isMobileOrTablet ? "моб версія" : "деск версія"
+);
+</script>
