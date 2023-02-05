8006d4b792844ebcddac1d46b7af1ecdc33eddc7
yakovyshynVitalii <vakovisin94@gmail.com>
2023-02-05 17:26:38 +0200

    add swiper to project

---
+++ project/pages/swiper.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="swiper-section">
+    <swiper
+      :pagination="{
+        type: 'fraction',
+      }"
+      :navigation="true"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+      <swiper-slide>Slide 5</swiper-slide>
+      <swiper-slide>Slide 6</swiper-slide>
+      <swiper-slide>Slide 7</swiper-slide>
+      <swiper-slide>Slide 8</swiper-slide>
+      <swiper-slide>Slide 9</swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script setup>
+// структуру та роботу слайдера можна розгялнути https://swiperjs.com/vue
+// тут можна розгялнути готові слайдери https://swiperjs.com/demos
+// але їх синтаксис потрібно перевести в vue 3 composition Api
+
+// import Swiper core and required modules
+import { Navigation, Pagination } from "swiper";
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// Import Swiper styles
+
+const modules = [Pagination, Navigation];
+</script>
+
+<style>
+#app {
+  height: 100%;
+}
+html,
+body {
+  position: relative;
+  height: 100%;
+}
+
+body {
+  background: #eee;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  color: #000;
+  margin: 0;
+  padding: 0;
+}
+
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 500px;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
